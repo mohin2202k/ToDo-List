09375ba406a971a28af2801518aabc37688d0db7
Mohin Kumar <mohinkumar220208@gmail.com>
2025-05-15 23:46:14 +0530

    11.46 15/5

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const addTaskButton = document.querySelector('#addTask');
 let taskCheck = document.querySelectorAll('.taskCheck');
 let deleteBtn = document.querySelectorAll('.deleteBtn');
 let taskList = document.querySelector('.taskList');
+let comment = document.querySelector('.comment');
 
 let tasks = [];
 
@@ -20,11 +21,22 @@ let addTask = function () {
     taskList.append(taskDiv);
     let delbtn = taskDiv.querySelector('.deleteBtn');
     addELdel(delbtn);
+    if (tasks.length === 0) {
+        comment.hidden = false;
+    } else {
+        comment.hidden = true;
+    }
 }
 
 let deleteTask = function (e) {
     let taskDiv = e.target.parentElement;
     taskList.removeChild(taskDiv);
+    tasks.pop(e.target.previousElementSibling.innerText);
+    if (tasks.length === 0) {
+        comment.hidden = false;
+    } else {
+        comment.hidden = true;
+    }
 }
 
 addTaskButton.addEventListener('click', addTask);
