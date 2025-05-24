2c072beafc5d4b542771597c82d099d44df947b5
Mohin Kumar <mohinkumar220208@gmail.com>
2025-05-24 22:52:32 +0530

    changed input style
    changed input style to to add from a separate input field

--- script.js
+++ script.js
@@ -3,29 +3,28 @@ let taskCheck = document.querySelectorAll('.taskCheck');
 let deleteBtn = document.querySelectorAll('.deleteBtn');
 let taskList = document.querySelector('.taskList');
 let comment = document.querySelector('.comment');
+let taskInput = document.querySelector('#taskInput');
 
 let tasks = [];
 let taskNum = 0;
 
 let addTask = function () {
+    if (taskInput.value === "") {
+        alert("Please enter a task");
+        return;
+    }
     let taskDiv = document.createElement('div');
     taskDiv.classList.add('taskDiv');
     taskNum++;
     taskDiv.innerHTML = `
                 <input type="checkbox" class="taskCheck">
-                <input type="text" class="taskText" placeholder="Enter a task">
-                <button class="deleteBtn"><img src="garbageCan.svg" alt=""></button>`;
+                <span class="taskText">${taskInput.value}</span>  
+                <button class="deleteBtn"><img src="garbageCan.svg" alt=""></button>
+                `;
     taskList.append(taskDiv);
-    let targetTask = taskDiv.querySelector('.taskText').focus();
     let delbtn = taskDiv.querySelector('.deleteBtn');
     addELdel(delbtn);
-    let checkbtn = taskDiv.querySelector('.taskCheck');
-    addELcheck(checkbtn);
-    if (taskNum === 0) {
-        comment.style.display = "block";
-    } else {
-        comment.style.display = "none";
-    }
+    taskInput.value = "";
 }
 
 let deleteTask = function (e) {
@@ -39,14 +38,6 @@ let deleteTask = function (e) {
     }
 }
 
-let disableInput = function (e) {
-    let taskText = e.target.parentElement.querySelector('.taskText');
-    if (e.target.checked) {
-        taskText.setAttribute('disabled', 'disabled');
-    } else {
-        taskText.removeAttribute('disabled');
-    }
-}
 
 addTaskButton.addEventListener('click', addTask);
 
